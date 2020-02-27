ffe35cb766356e1e2a5528478121d31ec76e4658
Bharat Parmar <bharat.parmar@goals101.in>
2020-02-28 01:33:39 +0530

    done

--- pkg/apis/samplecrd/v1/register.go
+++ pkg/apis/samplecrd/v1/register.go
@@ -1,21 +1,22 @@
 package v1
 
 import (
-	"github.com/bks1989/k8s-controller-custom-resource/pkg/apis/samplecrd"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
+
+	"github.com/bks1989/k8s-controller-custom-resource/pkg/apis/samplecrd"
 )
 
-// GroupVersion is the identifer for API which includes
-// the name of the group and the version for the API
+// GroupVersion is the identifier for the API which includes
+// the name of the group and the version of the API
 var SchemeGroupVersion = schema.GroupVersion{
 	Group:   samplecrd.GroupName,
 	Version: samplecrd.Version,
 }
 
-// create schemaBuilder which uses function to add types
-// the schema
+// create a SchemeBuilder which uses functions to add types to
+// the scheme
 var (
 	SchemeBuilder = runtime.NewSchemeBuilder(addKnownTypes)
 	AddToScheme   = SchemeBuilder.AddToScheme
@@ -26,23 +27,20 @@ func Resource(resource string) schema.GroupResource {
 	return SchemeGroupVersion.WithResource(resource).GroupResource()
 }
 
-// kind taks an unqualifed kind and return Grup qualified GroupKind
+// Kind takes an unqualified kind and returns back a Group qualified GroupKind
 func Kind(kind string) schema.GroupKind {
 	return SchemeGroupVersion.WithKind(kind).GroupKind()
 }
 
-// addKnowTpes add our tyes to the API scheme by registering
+// addKnownTypes adds our types to the API scheme by registering
 // Network and NetworkList
-// func addKnownTypes(schema *runtime.Scheme) error {
-// 	schema.AddKnownTypes()
-// }
-
 func addKnownTypes(scheme *runtime.Scheme) error {
 	scheme.AddKnownTypes(
 		SchemeGroupVersion,
 		&Network{},
 		&NetworkList{},
 	)
+
 	// register the type in the scheme
 	metav1.AddToGroupVersion(scheme, SchemeGroupVersion)
 	return nil
--- pkg/apis/samplecrd/v1/types.go
+++ pkg/apis/samplecrd/v1/types.go
@@ -6,11 +6,11 @@ import (
 
 // +genclient
 // +genclient:noStatus
-// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinary/pkg/runtime.Object
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 
-// Network describe a Network resource
+// Network describes a Network resource
 type Network struct {
-	// TypeMeta is the metadata for the resource , like kind and apiVersion
+	// TypeMeta is the metadata for the resource, like kind and apiversion
 	metav1.TypeMeta `json:",inline"`
 	// ObjectMeta contains the metadata for the particular object, including
 	// things like...
@@ -20,11 +20,16 @@ type Network struct {
 	//  - labels
 	//  - ... etc ...
 	metav1.ObjectMeta `json:"metadata,omitempty"`
-	Spec              NetworkSpec `json:"spec"`
+
+	// Spec is the custom resource spec
+	Spec NetworkSpec `json:"spec"`
 }
 
-// NetworkSpect is the spec for network resource
+// NetworkSpec is the spec for a Network resource
 type NetworkSpec struct {
+	// Cidr and Gateway are example custom spec fields
+	//
+	// this is where you would put your custom resource data
 	Cidr    string `json:"cidr"`
 	Gateway string `json:"gateway"`
 }
